f4601142fb3c97b8624b7a1bca9bd4883923b2b6
thecodearcher <brian.iyoha@gmail.com>
2019-01-02 09:38:13 +0100

    changed console log to logger

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import app from "./app";
 import { PORT } from "./config";
+import { logger } from "./utils/logger";
 
 const port = PORT || 3000;
 app.set("port", port);
 app.listen(app.get("port"), (err) => {
     if (err) {
-        return console.error(err);
+        return logger.error(err);
     }
 
-    return console.log(`server is listening on ${port}`);
+    return logger.info(`server is listening on ${port}`);
 });
